7926921c0569580e751be9f7251e438215201c27
murphypicard <ayaghsizian@gmail.com>
2017-05-19 18:44:21 -0400

    angular controller test OK

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('js is connected');
+
+angular
+  .module("mainApp", [])
+  .controller("mainCTRL", mainCTRL);
+
+function mainCTRL ($scope){
+  $scope.greeting = "hello";
+
+  $scope.info = {
+    name: ["Ara", 'bob'],
+    framework: ['AngularJS', 'React'],
+    emotion: ['Love']
+  };
+}
