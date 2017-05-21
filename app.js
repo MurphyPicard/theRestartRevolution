91e13285322ebd073805f9a4a76fb1df0f84e147
murphypicard <ayaghsizian@gmail.com>
2017-05-21 00:29:41 -0400

    rzslider not recognized going to bed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ angular
   .controller("mainCTRL", mainCTRL);
 
 function mainCTRL ($scope){
+
   $scope.greeting = ['hi', 'hello', 'how are u'];
 
   $scope.info = {
@@ -12,4 +13,16 @@ function mainCTRL ($scope){
     framework: ['AngularJS', 'React'],
     emotion: ['Love']
   };
-}
+
+  $scope.slider = {
+    minValue: 10,
+    maxValue: 90,
+    options: {
+      floor: 0,
+      ceil: 100,
+      step: 10,
+      showTicks: true
+    }
+  };
+
+}//mainCTRL
